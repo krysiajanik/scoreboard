65ea01872ec203b82ba15459b0b8752af44d6775
Krysia Janik <a.krystyna.janik@gmail.com>
2020-07-31 13:35:14 +0200

    feat: increasing and decreasing the date works on the `selected date`

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -2,30 +2,36 @@ var moment = require('moment');
 moment().format();
 
 
-// let highlightDay = document.querySelector(".date__selected--js");
-
+let highlightDay = document.querySelector(".date__selected--js");
 // let dayMinusOne = document.querySelector(".date__minus-one--js");
-
 // let dayMinusTwo = document.querySelector(".date__minus-two--js");
-
 // let dayPlusOne = document.querySelector(".date__plus-one--js");
-
 // let dayPlusTwo = document.querySelector(".date__plus-two--js");
 
-
-
-const nowObj = { day: moment().format("D"), month: moment().format("MMMM"), year: moment().format("YYYY") };
+//const nowObj = { day: moment().format("D"), month: moment().format("MMMM"), year: moment().format("YYYY") };
 let now = moment().format("D, MMMM, YYYY");
 console.log(now);
 
+//create the object with correct date
+let nowO = (date) => {
+  let nowObj = { day: moment(date).format("D"), month: moment(date).format("MMMM"), year: moment(date).format("YYYY") };
+  return nowObj
+}
+
 
 //Function for increasing the date by one day
+let nowP = (date) => {
+  let newDate = moment(date).add(1, 'd');
+  sessionStorage.setItem('selectedDate', newDate);
+  return newDate;
+};
+
+//Function for changing the html view and adding storage item
 function nowPlus() {
-  let nowP = (date) => {
-    let newDate = moment(date).add(1, 'd');
-    sessionStorage.setItem('selectedDate', newDate);
-    return newDate;
-  };
+  let { day, month, year } = nowO(now);
+  const dateSelected = `<p class="date__year year__selected--js">${year}</p>
+      <p class="date__month month__selected--js">${month}</p>
+      <p class="date__day day__selected--js">${day}</p>`
 
   if (sessionStorage.getItem('selectedDate') == true) {
     let selectedDate = sessionStorage.getItem('selectedDate');
@@ -33,32 +39,38 @@ function nowPlus() {
   } else {
     now = nowP(now);
   }
+  highlightDay.innerHTML = dateSelected
 }
 
 //get the button and add event listener
 const buttonFollowing = document.querySelector(".date__following--js");
-
 buttonFollowing.addEventListener('click', nowPlus);
 
-//Function for decreasing the date by one day
-function nowMinus() {
-  let nowM = (date) => {
-    let newDate = moment(date).subtract(1, 'd');
-    sessionStorage.setItem('selectedDate', newDate);
-    return newDate;
-  };
 
-  if (sessionStorage.getItem('selectedDate') == true) {
+//Function for decreasing the date by one day
+let nowM = (date) => {
+  let newDate = moment(date).subtract(1, 'd');
+  sessionStorage.setItem('selectedDate', newDate);
+  return newDate;
+};
+
+//Function for changing the html view and adding storage item
+function nowMinus() {
+  let { day, month, year } = nowO(now);
+  const dateSelected = `<p class="date__year year__selected--js">${year}</p>
+      <p class="date__month month__selected--js">${month}</p>
+      <p class="date__day day__selected--js">${day}</p>`
+
+    if (sessionStorage.getItem('selectedDate') == true) {
     let selectedDate = sessionStorage.getItem('selectedDate');
     now = nowM(selectedDate);
   } else {
     now = nowM(now);
   }
+  highlightDay.innerHTML = dateSelected
 }
-
 
 //get the button and add event listener
 const buttonPrevious = document.querySelector(".date__previous--js");
-
 buttonPrevious.addEventListener('click', nowMinus);
 
