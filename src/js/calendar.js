a6a2871fff60a2751acd368084d0e432083dc337
Krysia Janik <a.krystyna.janik@gmail.com>
2020-07-31 09:04:15 +0200

    feat: function for decreasing one day added to calendar.js

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -19,14 +19,14 @@ let now = moment().format("D, MMMM, YYYY");
 console.log(now);
 
 
-
+//Function for increasing the date by one day
 function nowPlus() {
-  let nowP = (date) => { 
-    let result = moment(date).add(1, 'd');
-    sessionStorage.setItem('selectedDate', result);
-    return result;
+  let nowP = (date) => {
+    let newDate = moment(date).add(1, 'd');
+    sessionStorage.setItem('selectedDate', newDate);
+    return newDate;
   };
-  
+
   if (sessionStorage.getItem('selectedDate') == true) {
     let selectedDate = sessionStorage.getItem('selectedDate');
     now = nowP(selectedDate);
@@ -35,9 +35,30 @@ function nowPlus() {
   }
 }
 
+//get the button and add event listener
 const buttonFollowing = document.querySelector(".date__following--js");
 
-buttonFollowing.addEventListener('click', nowPlus)
+buttonFollowing.addEventListener('click', nowPlus);
+
+//Function for decreasing the date by one day
+function nowMinus() {
+  let nowM = (date) => {
+    let newDate = moment(date).subtract(1, 'd');
+    sessionStorage.setItem('selectedDate', newDate);
+    return newDate;
+  };
+
+  if (sessionStorage.getItem('selectedDate') == true) {
+    let selectedDate = sessionStorage.getItem('selectedDate');
+    now = nowM(selectedDate);
+  } else {
+    now = nowM(now);
+  }
+}
 
 
+//get the button and add event listener
+const buttonPrevious = document.querySelector(".date__previous--js");
+
+buttonPrevious.addEventListener('click', nowMinus);
 
