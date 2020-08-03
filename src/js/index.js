37ae72bc63d3b072be34e18db0381849114e2a5e
Krysia Janik <a.krystyna.janik@gmail.com>
2020-08-03 15:49:12 +0200

    feat: firebase added to the project

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,15 @@ import '../scss/main.scss';
 import {registerSW} from './pwa.js';
 registerSW();
 
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: 'AIzaSyBT3NVs3JliyXgrV2ZP9ef9z3W9Hip2JVc',
+  authDomain: 'scoreboard-072020.firebaseapp.com',
+  projectId: 'scoreboard-072020'
+});
+
+var db = firebase.firestore();
